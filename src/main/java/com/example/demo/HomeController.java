fe4ce520ae7987c40341e3ab8d3105b5240d6ebe
yas-ds <yas.ds@hotmail.com>
2019-08-19 14:40:09 -0400

    descriptive message

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping ("/")
-    public String homePage(Model model){
+    @RequestMapping("/")
+    public String homePage(Model model) {
         model.addAttribute("myvar", "Say hello to the people.");
         return "hometemplate";
+    }
+
+
+    @RequestMapping("/next")
+    public String nextpage(Model model) {
+        model.addAttribute("myvar2", "Welcome to Java BootCamp.");
+        return "nextpagetemplate";
     }
 }
